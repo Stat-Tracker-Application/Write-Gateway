4073842c66ad0cb92e57501eea276d783aa294d9
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2023-11-27 09:25:43 +0100

    Databases set up

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,35 +1,16 @@
 import express from "express";
 import axios from "axios";
-// import https from "https";
-// import fs from "fs";
-import {
-  createProxyMiddleware,
-  Filter,
-  Options,
-  RequestHandler,
-} from "http-proxy-middleware";
 
 const router = express.Router();
 
-// const userapibasestring = "https://localhost:5000/";
-// const statapibasestring = "https://localhost:6000/";
+const userapibasestring = "http://User-Api:5200/";
+const statapibasestring = "http://Stat-Api:5100/";
 
-const userapibasestring = "http://localhost:5000/";
-const statapibasestring = "http://localhost:6000/";
-
-// const myhttpsagent = new https.Agent({
-//   ca: fs.readFileSync("./Certs/ca.crt"),
-//   key: fs.readFileSync("./Certs/localhost.key"),
-//   cert: fs.readFileSync("./Certs/localhost.crt"),
-// });
-
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //dirty fix as discussed to get around the cert issues
+router.all("/", function (req, res) {
+  res.send("Hello world from gateway");
+});
 
 router.all("/userapi", function (req, res) {
-  // if (!req.client.authorized) {
-  //   res.writeHead(401);
-  //   return res.end("Invalid client certificate authentication.");
-  // }
   console.log("Sending a request to the user api");
   axios.get(`${userapibasestring}`).then(function (response) {
     res.json(response.data).send();
@@ -37,18 +18,10 @@ router.all("/userapi", function (req, res) {
 });
 
 router.all("/statapi", function (req, res) {
-  // if (!req.client.authorized) {
-  //   res.writeHead(401);
-  //   return res.end("Invalid client certificate authentication.");
-  // }
   console.log("Sending a request to the stat api");
   axios.get(`${statapibasestring}`).then(function (response) {
     res.json(response.data).send();
   });
-});
-
-router.all("/", function (req, res) {
-  res.send("Hello world from gateway");
 });
 
 export default router;
